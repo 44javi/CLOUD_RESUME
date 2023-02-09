ad4b80696e082c503c3fb698c0dcf03dcc6ee1d1
javier pacheco <pachecojavier44@gmail.com>
2023-02-08 19:30:48 -0800

    Updated JavaSript, C sharp, API

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,19 +1,23 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
-    getVisitCount();
-})
+window.addEventListener("DOMContentLoaded", (event) => {
+  getVisitCount();
+});
 
-const functionApi = '';
+const functionApi = "http://localhost:7071/api/GetResumeCounter";
 
-const getVisitCount = () =>{
-    let count = 30;
-    fetch(functionApi).then(response => {
-        return response.json()
-    }).then (response =>{
-        console.log("Website called function API.");
-        count = response.count;
-        document.getElementById("counter"),innerText = count;
-    }).catch(function(error){
-        console.log(error);
+const getVisitCount = () => {
+  let count = 30;
+  fetch(functionApi)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log("Website called function API.");
+      count = response.count;
+      document.getElementById("counter").innerText = count;
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-    return count;
-}
+  return count;
+};
+
